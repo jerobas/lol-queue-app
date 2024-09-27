a4d61f6ce85a336ab68730e1fec8b51a213c2496
flaviozno <flavio.vezono@ufu.br>
2024-09-27 09:03:28 -0300

    ico

--- src/msi.js
+++ src/msi.js
@@ -4,26 +4,28 @@ const fs = require("fs");
 
 const packageJson = require(path.join(__dirname, "../package.json"));
 
-
-const APP_DIR = path.join(__dirname, "../", "build");
+const APP_DIR = path.join(__dirname, "../", "build", "win-unpacked");
+const BIULD_DIR = path.join(__dirname, "../", "build");
 const OUT_DIR = path.join(__dirname, "../", "msi-installer");
+const ICON_DIR = path.join(__dirname, "lol.ico");
 
 const msiCreator = new MSICreator({
   appDirectory: APP_DIR,
   outputDirectory: OUT_DIR,
   description: packageJson.description,
-  exe: packageJson.build.msi.shortcutName,
-  name: packageJson.build.msi.shortcutName,
+  exe: packageJson.build.productName,
+  name: packageJson.build.productName,
   manufacturer: "Jerobas",
   version: packageJson.version,
   ui: {
     chooseDirectory: true,
   },
+  icon: ICON_DIR,
 });
 
 msiCreator.create().then(function () {
   msiCreator.compile().then(() => {
-    fs.rm(APP_DIR, { recursive: true, force: true }, (err) => {
+    fs.rm(BIULD_DIR, { recursive: true, force: true }, (err) => {
       if (err) console.log(`Erro for delete build folder: ${err}`);
       else console.log("Build folder was deleted");
     });
